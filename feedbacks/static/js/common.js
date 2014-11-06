983c29a15ee0a4290acd4dac0d074ad3e0c1a47a
VovanR <mail@vovanr.com>
2014-11-06 22:19:23 +0300

    Require common.js and form scripts

--- feedbacks/static/js/common.js
+++ feedbacks/static/js/common.js
@@ -4,17 +4,15 @@
 
 // Тесты имеют свой базовый путь, поэтому так:
 if (!requirejs.config.baseUrl) {
-    requirejs.config.baseUrl = '../js/';
+    requirejs.config.baseUrl = '/static/';
 }
 
 // Пути к библиотекам, типа jQuery
 requirejs.config({
     baseUrl: requirejs.config.baseUrl, // Путь к статике
     paths: {
-        jquery: '../vendor/jquery/dist/jquery',
-        jqueryCookie: '../vendor/jquery.cookie/jquery.cookie',
-
-        'dj-feedback': './dj-feedback',
+        jquery: './vendor/jquery/dist/jquery',
+        jqueryCookie: './vendor/jquery.cookie/jquery.cookie',
     },
     shim: {
         jqueryCookie: ['jquery'],
