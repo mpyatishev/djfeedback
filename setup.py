ab8e2e9ebe9c068ca5761b818a2ca3491b2691b0
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2014-11-10 15:36:39 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'Django >= 1.6',
+    'django-compressor >= 1.4',
+    'django-bootstrap3',
+    'python-redmine',
+]
 
 
 setup(
-    name="djfeedback",
+    name='djfeedback',
     version='0.0.1',
     description='django feedback app',
     author='mpyatishev',
     author_email='mpyatishev@gmail.com',
-    packages=['feedbacks'],
-    install_requires=[],
+    packages=find_packages(exclude=[
+        'example',
+    ]),
+    package_data={
+        '': [
+            'bower.json',
+            'gulpfile.js',
+            'package.json',
+        ],
+        'feedbacks': [
+            'templates/*',
+            'static/dj-feedback.build.js',
+            'static/js/*',
+            'static/styl/*',
+        ]
+    },
     license="MIT",
     url='https://github.com/mpyatishev/djfeedback',
     classifiers=[
@@ -20,5 +42,7 @@ setup(
         "License :: OSI Approved :: MIT",
         "Programming Language :: Python :: 2.7",
         "Topic :: Development",
-    ]
+    ],
+    zip_safe=False,
+    install_requires=install_requires
 )
