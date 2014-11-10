c1f632f5f1c64cc558e040b911965dd849305dd1
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2014-11-10 12:27:31 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name="djfeedback",
+    version='0.0.1',
+    description='django feedback app',
+    author='mpyatishev',
+    author_email='mpyatishev@gmail.com',
+    packages=['djfeedback'],
+    install_requires=[],
+    license="MIT",
+    long_description='https://github.com/mpyatishev/djfeedback',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Development",
+        "License :: OSI Approved :: MIT",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Development",
+    ]
+)
