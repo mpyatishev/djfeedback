9c3c5d9c4e8620fe3fcc25044ef6b3fd5cbf3e8c
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2014-11-12 17:09:45 +0300

    add custom field email; add assigned_to

--- feedbacks/backends/redmine_backend.py
+++ feedbacks/backends/redmine_backend.py
@@ -18,9 +18,18 @@ class RedmineBackend(IBackend):
         if tracker is None:
             tracker = 'bug'
 
+        email_field_id = settings.DJFEEDBACK_REDMINE_EMAIL_FIELD_ID
+        custom_fields = [
+            {
+                'id': email_field_id,
+                'value': message.email
+            }
+        ]
         self.redmine.issue.create(
             project_id=self.project_id,
             tracker_id=settings.DJFEEDBACK_REDMINE_TRACKERS[tracker],
             subject=message.subj,
             description=message.text,
+            assigned_to_id=settings.DJFEEDBACK_REDMINE_ASSIGN_TO_ID,
+            custom_fields=custom_fields
         )
--- feedbacks/models.py
+++ feedbacks/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class Feedback(models.Model):
     date = models.DateTimeField(_('date'), auto_now_add=True)
     name = models.CharField(_('name'), max_length=254, blank=True, null=True)
-    email = models.EmailField(_('email'), max_length=254)
+    email = models.EmailField(_('e-mail'), max_length=254)
     ftype = models.ForeignKey('FeedbackType')
     subj = models.CharField(_('subject'), max_length=254)
     text = models.TextField(_('text'))
